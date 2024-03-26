25b6ff8ddd68d3b3521b774a63cdc31e989bbad5
21-manic-016g <21-manic-016g@21-manic-016>
2024-03-26 15:44:53 +0900

    hi

---
+++ Baekjoon2024/src/P2024/P2471B.java
@@ -0,0 +1,18 @@
+package P2024;
+	import java.util.Scanner;
+public class P2471B {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			int N= sc.nextInt();
+			sc.close();
+			
+			int i =1;
+			
+			while(i<=N) {
+				System.out.println(i);
+				i++;
+			}
+		}
+}
---
+++ Baekjoon2024/src/P2024/P2741.java
@@ -0,0 +1,20 @@
+package P2024;
+
+import java.util.Scanner;
+
+public class P2741 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		int i;
+		
+		for(i=0;i<=N;N++) {
+	//	while () {
+				System.out.println(N);
+				{
+	}
+}
+}
+}
