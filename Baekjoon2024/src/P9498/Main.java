350ae0ead43b398863507d887f1d81f4330a9263
21-manic-016g <21-manic-016g@21-manic-016>
2024-03-27 16:41:29 +0900

    c

---
+++ Baekjoon2024/src/P9498/Main.java
@@ -0,0 +1,23 @@
+package P9498;
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int score =sc.nextInt();
+		sc.close();
+		
+		if(score>=90) {
+			System.out.println("A");
+		} else if(score>=80)
+			System.out.println("B");
+		  else if(score>=70) {
+			System.out.println("C");
+	}     else if(score>=60) {
+		    System.out.println("D");
+	} else {
+		   System.out.println("F");
+}
+}
+}
