f69883e1146160e785998db3693cffa2c4f4ec8b
21-manic-016g <21-manic-016g@21-manic-016>
2024-04-16 17:49:05 +0900

    c

---
+++ Baekjoon2024/src/p1193/Main.java
@@ -0,0 +1,22 @@
+package p1193;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int rightv = 0;
+		int leftv = 0;
+		int a = sc.nextInt();
+		sc.close();
+	//	
+		for(int i=0; i<a;i++) {
+			rightv++;
+			
+			for(int j=0; i<j; i++) {
+			System.out.println(a);
+			}
+		}
+	}
+}
