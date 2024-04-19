031395fbc3c456fb6adac300007f01e285e168ce
21-manic-016g <21-manic-016g@21-manic-016>
2024-04-19 17:33:08 +0900

    cd

---
+++ Baekjoon2024/src/P10871/Main.java
@@ -0,0 +1,25 @@
+package P10871;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int N = sc.nextInt();
+		
+		for(int i=0; i<A;i++) {
+		int X = sc.nextInt();
+		
+	//	System.out.println(X);
+		if(X<N ) {
+			System.out.print(X + " ");
+		}else {
+			
+		}
+
+	//	System.out.print(N); && X < N
+	}
+}
+}
---
+++ Baekjoon2024/src/P10871/Suc.java
@@ -0,0 +1,12 @@
+package P10871;
+
+public class Suc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
