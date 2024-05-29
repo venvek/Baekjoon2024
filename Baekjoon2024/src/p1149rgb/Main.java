f7fd2ed645462841e2d0693589b8543727a1b621
21-manic-016g <21-manic-016g@21-manic-016>
2024-05-29 14:38:25 +0900

    0529 김진성

---
+++ Baekjoon2024/src/p1149rgb/Main.java
@@ -0,0 +1,17 @@
+package p1149rgb;
+
+import java.util.Scanner;
+
+public class Main {
+
+	static int a;
+	static int[][] cost;
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int N = sc.nextInt();
+	cost = new int [N][3];
+	System.out.println(cost);
+	
+	}
+}
